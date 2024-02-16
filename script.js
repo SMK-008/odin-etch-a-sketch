8b0297776b9e8774750f244301fae247f6cb1c3e
SMK-008 <sammy.kitonga@strathmore.edu>
2024-02-16 14:09:00 +0300

    created new game and delete functionalities

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
+const container=document.querySelector('.container')
+//creating the grid squares
 let rows=16
 let columns=16
-
 const grid=document.querySelector('.grid')
-
 for(let i=0;i<rows;i++){
     for(let j=0;j<columns;j++){
         const gridItem=document.createElement('div')
         gridItem.className="grids"
-        // gridItem.textContent=`Cell ${i+1}-${j+1}`
 
         grid.appendChild(gridItem)
     }
 }
 
 
+//new game functionality
+
+const newgame=document.querySelector('#new')
+newgame.addEventListener('click',()=>{
+    let newGrid=document.createElement('div')
+    newGrid.className='new'
+
+    let width=prompt("What width would you like?")
+    let length=prompt("What length would you like")
+    if(width<=100 && length<=100 || width>0 && length>0){
+        for(let i=0;i<width;i++){
+            for(let j=0;j<length;j++){
+                const gridItem=document.createElement('div')
+                gridItem.className="new-grids"
+                
+                newGrid.appendChild(gridItem)
+            }
+        }
+        container.append(newGrid)
+    }else{
+        alert('Please enter values less than 101 or that are not negative')
+    }
+})
+
+
+//delete game functionality
+const del=document.querySelector('#delete')
+del.addEventListener('click',()=>{
+    let newGrid=document.querySelector('.new')
+    let grid=document.querySelector('.grid')
+    if(grid!=null){
+        container.removeChild(grid)
+    }else{
+        container.removeChild(newGrid)
+    }
+})
+
+//hover functionality
 const grids=document.querySelectorAll('.grids')
 grids.forEach((item)=>{
     item.addEventListener('mouseover',function(e){
@@ -21,3 +58,6 @@ grids.forEach((item)=>{
         console.log(e)
     })
 })
+
+
+//hover functionality for the new grids
